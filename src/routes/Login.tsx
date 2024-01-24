e948d9d1f18fc51c8b2c6557b5c1b193b92f361f
timonstergg <daniel20011123@gmail.com>
2024-01-24 14:26:06 -0500

    segunda actualizacion (index css)

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,15 +1,22 @@
+import DefaultLayout from "../layout/DefaultLayout"
 export default function Login(){
-    return <form>
-        <h1>Login</h1>
-        <label>Username</label>
-        <input type="text"/>
+    return(
+        <DefaultLayout>
+            <form className="form">
+                 <h1>Login</h1>
+                  <label>Username</label>
+                 <input type="text"/>
         
-        <label>Password</label>
-        <input type="password"/>
+                  <label>Password</label>
+                 <input type="password"/>
 
-        <button>
-            login
-        </button>
+                 <button>
+                   login
+                 </button>
 
-    </form>
+             </form>
+
+        </DefaultLayout>
+    
+    )
 }
